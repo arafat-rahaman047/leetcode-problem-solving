fe3efb59c5f2fde9670931d3513b244ae46de532
arafat-rahaman047 <arafat.cse10.bu@gmail.com>
2026-05-14 03:02:30 +0600

    Commit after saving

---
+++ Sorting/88_Merge_Sorted_Array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+    }
+};
+
+int main() {
+  vector<int> nums1 = {1,2,3,0,0,0};
+  vector<int> nums2 = {2,5,6};
+
+  int m = 3;
+  int n = 3;
+
+  Solution obj;
+
+  obj.merge(nums1, m, nums2, n);
+
+  return 0;
+}
