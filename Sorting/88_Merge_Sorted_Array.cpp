337668f79c1a81b5399ba78ffffc588e1d8c835f
arafat-rahaman047 <arafat.cse10.bu@gmail.com>
2026-05-14 12:54:44 +0600

    adding 88

--- Sorting/88_Merge_Sorted_Array.cpp
+++ Sorting/88_Merge_Sorted_Array.cpp
@@ -4,7 +4,42 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector <int> result;
+        int size1 = nums1.size(), size2 = nums2.size();
+        int i=0, j=0;
+
+        while(i<size1 && j<size2){
+          while(nums1[i]==0) i++;
+          while(nums2[j]==0) j++;
+
+          if(nums1[i]<=nums2[j]){
+            result.push_back(nums1[i]);
+            i++;
+          }
+          else{
+            result.push_back(nums2[j]);
+            j++;
+          }
+        }
+
+        if(i==size1-1){
+          while(j<size2){
+            while(nums2[j]==0) j++;
+            result.push_back(nums2[j]);
+            j++;
+          }
+        }
+
+        if(j==size2-1){
+          while(nums1[i]==0) i++;
+          result.push_back(nums1[i]);
+          i++;
+        }
         
+        int size3 = result.size();
+        for(int i=0;i<size3-1;i++){
+          nums1[i] = result[i]; 
+        }
     }
 };
 
@@ -19,5 +54,8 @@ int main() {
 
   obj.merge(nums1, m, nums2, n);
 
+  for(auto x: nums1){
+    cout<<x<<" ";
+  }
   return 0;
 }
