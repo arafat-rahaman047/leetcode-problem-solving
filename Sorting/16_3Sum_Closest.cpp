9c470a9f9117face34b60535198ab26fa456956f
arafat-rahaman047 <arafat.cse10.bu@gmail.com>
2026-05-19 23:30:02 +0600

    adding 3Sum Closest

--- Sorting/16_3Sum_Closest.cpp
+++ Sorting/16_3Sum_Closest.cpp
@@ -4,17 +4,30 @@ using namespace std;
 class Solution {
   public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int min = INT_MAX;
-        for(int i=0;i<nums.size();i++){
-          
+        int diff = INT_MAX, sum=0;
+        sort(nums.begin(),nums.end());
+        for(int k=0;k<nums.size()-2;k++){
+          int i=k+1, j=nums.size()-1;
+          while(i<j){
+            int curr_sum = (nums[k]+nums[i]+nums[j]);
+
+            if(curr_sum==target) return curr_sum;
+            else if(abs(target-curr_sum)<diff){
+              diff = abs(target-curr_sum);
+              sum = curr_sum;
+            }
+
+            if(curr_sum<target) i++;
+            else j--;
+          }
         }
-        return min;
+        return sum;
     }
 };
 
 int main() {
-  vector<int> nums = {-1,2,1,-4};
-  int target = 1;
+  vector<int> nums = {4,0,5,-5,3,3,0,-4,-5};
+  int target = -2;
 
   Solution obj;
 
